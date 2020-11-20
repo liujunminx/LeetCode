c4c3e46e30cbae854da44f00f8218a043f0a0c4a
liujunmin <1792619448@qq.com>
2020-11-20 15:03:47 +0800

    两数之和链表

---
+++ src/com/leetcode/algorithm/easy/TwoSum.java
@@ -0,0 +1,107 @@
+package com.leetcode.algorithm.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ *
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ *
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ */
+public class TwoSum {
+
+    /**
+     * Brute Force
+     * @param nums integers
+     * @param target integer
+     * @return integers
+     */
+    public int[] twoSum01(int[] nums, int target){
+        int len = nums.length;
+        for (int i = 0; i < len; i++){
+            int temp = target - nums[i];
+            for (int j = len-1; j > i; j--){
+                if (temp == nums[j]){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * Two pass hashtable
+     * we reduce the look up time from O(n) to O(1) by trading space for speed;
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum02(int[] nums, int target){
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            hashMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if (hashMap.containsKey(complement) && hashMap.get(complement) != i){
+                return new int[]{i, hashMap.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * One pass hashtable
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum03(int[] nums, int target){
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if (hashMap.containsKey(complement)){
+                return new int[]{i, hashMap.get(complement)};
+            }
+            hashMap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{
+                2, 7, 11, 15
+        };
+        int target = 9;
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum01(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
---
+++ src/com/leetcode/algorithm/medium/AndTwoNumbers.java
@@ -0,0 +1,57 @@
+package com.leetcode.algorithm.medium;
+
+import com.sun.org.apache.bcel.internal.generic.LNEG;
+
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){this.val = val;}
+    ListNode(int val, ListNode next){this.val = val; this.next = next;}
+}
+public class AndTwoNumbers {
+        public static ListNode addTwoNumbers(ListNode p1, ListNode p2){
+            ListNode dummyHead = new ListNode(0);
+            ListNode p =p1, q = p2, curr = dummyHead;
+            int carry = 0;
+            while (p != null || q != null){
+                int x = (p != null) ? p.val : 0;
+                int y = (q != null) ? q.val : 0;
+                int sum = carry + x + y;
+                carry = sum / 10; // 近卫加一
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+                if (p != null) p = p.next;
+                if (q != null) q = q.next;
+            }
+            if (carry > 0){// 最后的进位
+                curr.next = new ListNode(carry);
+            }
+            return dummyHead.next;
+        }
+
+    public static void main(String[] args) {
+        ListNode p = new ListNode(9);
+        p.next = new ListNode(9);
+        ListNode q = new ListNode(9);
+        q.next = new ListNode(9);
+        q.next.next = new ListNode(9);
+        q.next.next.next = new ListNode(9);
+        ListNode result = addTwoNumbers(q, p);
+//        System.out.println(result.val);
+        while (result != null){
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+}
