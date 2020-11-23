ff8c2d4adcde97f7a9719e3d7b2af49be0c23032
liujunmin <1792619448@qq.com>
2020-11-23 18:40:48 +0800

    字符串动态规划

---
+++ src/com/leetcode/algorithm/dynamicprogramming/LongestPalindromicSubstring.java
@@ -0,0 +1,34 @@
+package com.leetcode.algorithm.dynamicprogramming;
+
+public class LongestPalindromicSubstring {
+    public static String longestPalindrome(String s){
+        int n = s.length();
+        if (n == 1) return s;
+        boolean dp[][] = new boolean[n][n];
+        for (int i = 0; i < n; i++){
+            dp[i][i] = true;// 单个子字符直接为true
+        }
+        int start = 0, end = 0;
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < i; j++){
+                if (s.charAt(i) == s.charAt(j)){// 第一个字符与最后一个字符相等，并找出[i,j]之间所有最小回文字符串
+                    if ( i - j == 1 || dp[j + 1][i - 1]){// i - j == 1表示这是最小两个字符回文串，dp为true表示往内扩散看子字符串是否为回文串
+                        dp[j][i] = true;// 该范围字符串为回文串
+                        if (end - start < i - j){
+                            start = j;
+                            end = i;
+                        }
+                    }
+                }else {
+                    dp[j][i] = false;
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static void main(String[] args) {
+        String target = "ioajengdabadcdcdadjieoadae";
+        System.out.println(longestPalindrome(target));
+    }
+}
