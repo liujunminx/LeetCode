1f151b2e325badd812ba45e58feb51d0b2379ab0
liujunmin <1792619448@qq.com>
2021-01-02 20:43:25 +0800

    Swap Nodes in Pairs

---
+++ src/com/leetcode/algorithm/linkedlist/SwapNodesInPairs.java
@@ -0,0 +1,25 @@
+package com.leetcode.algorithm.linkedlist;
+
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head){
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        while (prev.next != null && prev.next.next != null){
+            ListNode current = prev.next;
+            ListNode forward = current.next;
+            ListNode temp = forward.next;
+
+            prev.next = forward;
+            forward.next = current;
+            current.next = temp;
+
+            prev = current;
+        }
+
+        return dummy.next;
+    }
+
+
+
+}
