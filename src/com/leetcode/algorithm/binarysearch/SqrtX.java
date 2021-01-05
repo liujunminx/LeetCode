74d871457c7894e086247eec6825957dbdd702e2
liujunmin <1792619448@qq.com>
2021-01-05 14:54:46 +0800

    ClimbingStairs dp

--- src/com/leetcode/algorithm/binarysearch/SqrtX.java
+++ src/com/leetcode/algorithm/binarysearch/SqrtX.java
@@ -2,20 +2,20 @@ package com.leetcode.algorithm.binarysearch;
 
 public class SqrtX {
     public int mySqrt(int x){
-        int left = 1;
-        int right = x;
-        int mid = left + (left + right)/2;
-        // 排除小数部分只需要跟输入数字比大小即可
-        while (right - left > 1){
-            int seq = mid * mid;
-            if (seq == x)
-                return mid;
-            else if (seq > x)
-                right = mid;
+        if (x < 2)
+            return x;
+        int hi = x, low = 1;
+        long mid = low + ((hi - low) / 2);
+        while (hi - low > 1) {
+            long sq = mid * mid;
+            if (sq == x)
+                return (int) mid;
+            if (sq > x)
+                hi = (int) mid;
             else
-                right = mid;
-            mid = left + (left + right) / 2;
+                low = (int) mid;
+            mid = low + ((hi - low) / 2);
         }
-        return mid;
+        return (int) mid;
     }
 }
