180d963f28924fd275249270369a2bfaa8c843ed
liujunmin <1792619448@qq.com>
2021-01-19 22:01:32 +0800

    backtrace

---
+++ src/com/leetcode/algorithm/backtracking/SubsetsII.java
@@ -0,0 +1,36 @@
+package com.leetcode.algorithm.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SubsetsII {
+    List<List<Integer>> output = new ArrayList<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums){
+        Arrays.sort(nums);
+        backtrace(nums, 0, new LinkedList<>());
+        return output;
+    }
+
+    public void backtrace(int[] nums, int startIndex, LinkedList<Integer> buffer){
+        output.add(new ArrayList<>(buffer));
+        for (int i = startIndex; i < nums.length; i++){
+            if (i > startIndex && nums[i] == nums[i-1]) continue;
+
+            int num = nums[i];
+            buffer.add(num);
+
+            backtrace(nums, i + 1, buffer);
+            buffer.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{
+                1, 2, 2
+        };
+        List<List<Integer>> res = new SubsetsII().subsetsWithDup(nums);
+        System.out.println(res);
+    }
+}
--- src/com/leetcode/concurrency/PrintInOrder.java
+++ src/com/leetcode/concurrency/PrintInOrder.java
@@ -47,4 +47,6 @@ public class PrintInOrder {
         }
         printThird.run();
     }
+
+
 }
